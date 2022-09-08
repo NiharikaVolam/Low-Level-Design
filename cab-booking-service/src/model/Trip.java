521a3ce4cb0855722ac47a6d9bdb516e0ee55776
Niharika Volam <Niharika.Volam@blueyonder.com>
2022-09-08 14:58:52 +0530

    Added LMS

---
+++ cab-booking-service/src/model/Trip.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Trip {
+
+    Cab cab;
+    User rider;
+
+    Location source;
+    Location destination;
+    TripStatus status;
+
+    public User getRider() {
+        return rider;
+    }
+
+    public void setStatus(TripStatus status) {
+        this.status = status;
+    }
+
+
+
+
+}
